1bc23eb4e36cbdcdba96ebf04b754d8a376bf897
U-STAUBLIIT\avil <avil@CARL0354.it.staubli.pri>
2017-03-08 17:44:08 +0100

    library.properties

--- AVIL/avil.h
+++ AVIL/avil.h
@@ -1,7 +1,6 @@
 /* 
  * Copyright 2017 Alessio Villa
  * 
- * version: 0.1.0
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
